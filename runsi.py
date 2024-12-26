4d4ef7de40824ee3b60d9dbf273ad95fa81259d9
hominlim <limhomin@gmail.com>
2024-12-26 16:52:54 +0900

    일봉 데이터

--- runsi.py
+++ runsi.py
@@ -1,5 +1,6 @@
 import mojito
 import pprint
+import pandas as pd
 
 f = open("./mocke.key")
 lines = f.readlines()
@@ -15,6 +16,14 @@ broker = mojito.KoreaInvestment(
     mock=True
 )
 
+data=[]
+resp = broker.fetch_ohlcv(symbol="005930", timeframe='D', adj_price=True)
+# pprint.pprint(resp)
 
-resp = broker.fetch_price("005930")
-pprint.pprint(resp)
+df = pd.DataFrame(resp['output2'])
+dt = pd.to_datetime(df['stck_bsop_date'], format="%Y%m%d")
+df.set_index(dt, inplace=True)
+df = df[['stck_oprc', 'stck_hgpr', 'stck_lwpr', 'stck_clpr']]
+df.columns = ['open', 'high', 'low', 'close']
+df.index.name = "date"
+print(df)
