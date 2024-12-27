7ebaed6a7d5a1b14f91d04208c7c7af4aace6d6a
hominlim <limhomin@gmail.com>
2024-12-27 13:19:57 +0900

    데이터프레임

--- runsi.py
+++ runsi.py
@@ -16,14 +16,22 @@ broker = mojito.KoreaInvestment(
     mock=True
 )
 
-data=[]
-resp = broker.fetch_ohlcv(symbol="005930", timeframe='D', adj_price=True)
-# pprint.pprint(resp)
-
-df = pd.DataFrame(resp['output2'])
-dt = pd.to_datetime(df['stck_bsop_date'], format="%Y%m%d")
-df.set_index(dt, inplace=True)
-df = df[['stck_oprc', 'stck_hgpr', 'stck_lwpr', 'stck_clpr']]
-df.columns = ['open', 'high', 'low', 'close']
-df.index.name = "date"
+resp = broker.fetch_price("005930")
+# pprint.pprint(resp,)
+df = pd.DataFrame(resp)
 print(df)
+df.to_csv("result.csv")
+
+
+
+# data=[]
+# resp = broker.fetch_ohlcv(symbol="005930", timeframe='D', adj_price=True)
+# # pprint.pprint(resp)
+
+# df = pd.DataFrame(resp['output2'])
+# dt = pd.to_datetime(df['stck_bsop_date'], format="%Y%m%d")
+# df.set_index(dt, inplace=True)
+# df = df[['stck_oprc', 'stck_hgpr', 'stck_lwpr', 'stck_clpr']]
+# df.columns = ['open', 'high', 'low', 'close']
+# df.index.name = "date"
+# print(df)
